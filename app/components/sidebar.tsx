1656a82f35f8b10decf82673fcd4849bc12b3d16
周良 <wwwzhouliang@gmail.com>
2024-01-29 23:38:46 +0800

    no message

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT WebUI
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          在线版 ChatGPT
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
